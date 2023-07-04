e675e20fef565f8be8a0ec5a9f82f25ee974d28a
cinta <cintatafur@gmail.com>
2023-07-04 17:36:33 +0200

    📌 Create(card):creo card para realizar lista

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ShearchBoxComponent } from './components/shearch-box/shearch-box.component';
+import { HomePageComponent } from './pages/home/home.component';
+import { CardListComponent } from './components/card-list/card-list.component';
+
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HomePageComponent, ShearchBoxComponent, CardListComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports:[HomePageComponent]
 })
 export class GifsModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 /* Componentes */
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
+
 @NgModule({
   declarations: [SidebarComponent],
   imports: [CommonModule],
