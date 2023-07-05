2db7b100a8e1183aca63dd7a8b447608608cbbe6
cinta <cintatafur@gmail.com>
2023-07-05 11:54:16 +0200

    ⚡️ Interfaces(creo interfaces)

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({ providedIn: 'root' })
 export class GifsService {
+  public gifList: Gif[]=[];
   private _tagHistory: string[] = [];
   private apiKey: string = '5BuYHsXkXO3GW9k5oFaTlfSR5XiOGODO';
   private url: string = 'https://api.giphy.com/v1/gifs';
@@ -34,8 +36,9 @@ export class GifsService {
       .set('limit', '10')
       .set('q', tag);
 
-    this.http.get(`${this.url}/search`, { params }).subscribe((res) => {
-      console.log(res);
+    this.http.get<SearchResponse>(`${this.url}/search`, { params }).subscribe((res) => {
+      this.gifList = res.data;
+      console.log('Gifs: ' , this.gifList);
     });
   }
 }
