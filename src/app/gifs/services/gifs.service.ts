e4b4f0bc6247fcc2983c950facfbe7777aaf85ca
cinta <cintatafur@gmail.com>
2023-07-05 09:46:10 +0200

    🔧 Validación(service)

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -10,7 +10,23 @@ export class GifsService {
     return [...this._tagHistory];
   }
 
-  public searchTag(tag: string): void {
+  private organizeHistory(tag: string) {
+    /* Lo pasamos a minusculas */
+    tag = tag.toLowerCase();
+
+    /* Si el tagHistory incluye el tag nuevo lo elimino*/
+    if (this._tagHistory.includes(tag)) {
+      this._tagHistory = this._tagHistory.filter((oldtag) => oldtag !== tag);
+    }
+    /* y lo inserto */
     this._tagHistory.unshift(tag);
+    this._tagHistory = this.tagsHistory.splice(0, 10);
+  }
+
+  public searchTag(tag: string): void {
+    if (tag.length === 0) return;
+    this.organizeHistory(tag);
+
+    console.log(this._tagHistory);
   }
 }
