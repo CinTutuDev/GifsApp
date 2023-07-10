496c56eee9d8503c9e84a42aeaa2fcc1bcb344dc
cinta <cintatafur@gmail.com>
2023-07-10 14:29:27 +0200

    🔧 Create(componente):creo c para gifs

---
+++ src/app/gifs/components/gifs-card/gifs-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, Output, Input, OnInit } from '@angular/core';
+import { Gif } from '../../interfaces/gifs.interfaces';
+
+@Component({
+  selector: 'gifs-card',
+  templateUrl: './gifs-card.component.html',
+})
+export class GifsComponent implements OnInit {
+  @Input()
+  public gif!: Gif;
+
+  ngOnInit(): void {
+    if (!this.gif) throw new Error('Gif property is required');
+
+    throw new Error('Method not implemented.');
+  }
+}
--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -4,14 +4,16 @@ import { CommonModule } from '@angular/common';
 import { ShearchBoxComponent } from './components/shearch-box/shearch-box.component';
 import { HomePageComponent } from './pages/home/home.component';
 import { CardListComponent } from './components/card-list/card-list.component';
-
-
+import { GifsComponent } from './components/gifs-card/gifs-card.component';
 
 @NgModule({
-  declarations: [HomePageComponent, ShearchBoxComponent, CardListComponent],
-  imports: [
-    CommonModule
+  declarations: [
+    HomePageComponent,
+    ShearchBoxComponent,
+    CardListComponent,
+    GifsComponent,
   ],
-  exports:[HomePageComponent]
+  imports: [CommonModule],
+  exports: [HomePageComponent],
 })
-export class GifsModule { }
+export class GifsModule {}
